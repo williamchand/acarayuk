1626171da601c58beee0fd312413210039212be5
william chandra <williamchans57@gmail.com>
2024-06-26 18:11:57 +0700

    add animal repository

---
+++ src/main/java/com/amenodiscovery/authentication/service/RestService.java
@@ -0,0 +1,53 @@
+package com.amenodiscovery.authentication.service;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Collection;
+import java.util.Collections;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import org.springframework.http.HttpHeaders;
+
+@Service
+public class RestService {
+    private static Logger logger = LoggerFactory.getLogger(MovieService.class);
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    private Environment env;
+
+    public RestService() {
+        restTemplate = new RestTemplate();
+    }
+
+    public ResponseEntity<String> getTrakteerPlainJSON(int page) {
+        String uri = "https://api.trakteer.id/v1/public/supports";
+        UriComponents builder = UriComponentsBuilder.fromHttpUrl(uri)
+                            .queryParam("limit","5")
+                            .queryParam("page","1").build();
+        // create headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_XML);
+        // set `accept` header
+        headers.set("Accept", MediaType.APPLICATION_JSON.toString());
+        // set custom header
+        headers.set("Key", env.getProperty("app.trakteerSecret"));
+        headers.set("X-Requested-With", "XMLHttpRequest");
+        // build the request
+        HttpEntity<?> request = new HttpEntity<>(headers);
+        return restTemplate.exchange(builder.toUriString(), HttpMethod.GET, request, String.class);
+    }
+}
--- src/main/java/com/amenodiscovery/authentication/web/controller/AnimalController.java
+++ src/main/java/com/amenodiscovery/authentication/web/controller/AnimalController.java
@@ -5,6 +5,7 @@ import com.amenodiscovery.authentication.web.dto.GeneralDto;
 import com.amenodiscovery.authentication.persistence.model.Animal;
 import com.amenodiscovery.authentication.service.UserService;
 import com.amenodiscovery.authentication.service.AnimalService;
+import com.amenodiscovery.authentication.service.RestService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
@@ -25,11 +27,19 @@ public class AnimalController {
     AnimalService animalService;
 
     @Autowired
+    RestService restService;
+
+    @Autowired
     UserService accountService;
 
     @GetMapping("/v1/public/animal/recommend/{name}")
-    public ResponseEntity<GeneralDto<AnimalDto>> getRecommendation(Principal principal, @PathVariable("name") String name) {
+    public ResponseEntity<GeneralDto<AnimalDto>> getRecommendation(@PathVariable("name") String name) {
         Animal animal = animalService.getAnimalTypeByName(name.trim().replaceAll("\\s{2,}", " ").toLowerCase());
         return ResponseEntity.ok().body(GeneralDto.convertToDto(AnimalDto.convertToDto(animal)));
+    }
+
+    @GetMapping("/v1/public/trakteer/donation")
+    public ResponseEntity<String> getTrakteer(@RequestParam("page") int page) {
+        return restService.getTrakteerPlainJSON(page);
     }
 }
